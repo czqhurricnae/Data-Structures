3f50144c71ee68d61abc862f57be597a0c66f948
czqhurricane <7160844@qq.com>
2017-09-05 16:15:15 +0800

    链表的实现-算法精解-C语言实现

--- linkList.c
+++ linkList.c
@@ -1,3 +1,20 @@
+/**
+ *
+ * @copyright:
+ * @file: linkList.c
+ * @since: 2017-09-05
+ * @author: czq | 7160844@qq.com
+ * @description:
+ *    链表的函数实现 
+ * @usage:
+ * @others:
+ * @TODO:
+ * @Revision History:
+ * Date            Rel Ver.        Notes
+ * 2017-09-05        1.0             开始创建
+ *
+ **/
+
 #include <stdlib.h>
 #include <string.h>
 #include "linkList.h"
--- linkList.h
+++ linkList.h
@@ -1,3 +1,20 @@
+/**
+ *
+ * @copyright:
+ * @file: linkList.h
+ * @since: 2017-09-05
+ * @author: czq | 7160844@qq.com
+ * @description:
+ *    链表抽象数据类型的头文件 
+ * @usage:
+ * @others:
+ * @TODO:
+ * @Revision History:
+ * Date            Rel Ver.        Notes
+ * 2017-09-05        1.0             开始创建
+ *
+ **/
+
 #ifndef LINKLIST_H
 #define LINKLIST_H
 
