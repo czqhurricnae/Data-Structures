3c9865932f96d3ff52a670868d25eadb737aa82d
czqhurricane <7160844@qq.com>
2018-02-15 16:37:02 +0800

    bitree.h

--- bitree.h
+++ bitree.h
@@ -37,20 +37,20 @@ typedef struct BiTree_
 } BiTree;
 
 /* Public Interface */
-void bitree_init(BiTree *tree, void (*destroy)(void *data));
-void bitree_destroy(BiTree *tree);
-int bitree_ins_left(BiTree *tree, BiTreeNode *node, const void *data);
-int bitree_ins_right(BiTree *tree, BiTreeNode *node, const void *data);
-void bitree_rem_left(BiTree *tree, BiTreeNode *node);
-void bitree_rem_right(BiTree *tree, BiTreeNode *node);
-int bitree_merge(BiTree *merge, BiTree *left, BiTree *right, const void *data);
+void bitreeInit(BiTree *tree, void (*destroy)(void *data));
+void bitreeDestroy(BiTree *tree);
+int bitreeInsertLeft(BiTree *tree, BiTreeNode *node, const void *data);
+int bitreeInsertRight(BiTree *tree, BiTreeNode *node, const void *data);
+void bitreeRemoveLeft(BiTree *tree, BiTreeNode *node);
+void bitreeRemoveRight(BiTree *tree, BiTreeNode *node);
+int bitreeMerge(BiTree *merge, BiTree *left, BiTree *right, const void *data);
 
-#define bitree_size(tree) ((tree)->size)
-#define bitree_root(tree) ((tree)->root)
-#define bitree_is_eob(node) ((node) == NULL)
-#define bitree_is_leaf(node) ((node)->left == NULL && (node)->right == NULL)
-#define bitree_data(node) ((node)->data)
-#define bitree_left(node) ((node)->left)
-#define bitree_right(node) ((node)->right)
+#define bitreeSize(tree) ((tree)->size)
+#define bitreeRoot(tree) ((tree)->root)
+#define bitreeIsEob(node) ((node) == NULL)
+#define bitreeIsLeaf(node) ((node)->left == NULL && (node)->right == NULL)
+#define bitreeData(node) ((node)->data)
+#define bitreeLeft(node) ((node)->left)
+#define bitreeRight(node) ((node)->right)
 
 #endif    /*BITREE_H*/
